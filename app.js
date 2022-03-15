563079cf1d63434c4458fe7ae2a02ebb865a620b
Massimo <googmassimogoog@gmail.com>
2022-03-15 18:59:17 +0100

    Finished exercise RockPaperScissors via new branch

--- app.js
+++ app.js
@@ -27,46 +27,141 @@ function playerPlay() {
     
 }
 
+// create an evenlistener which get the id of the fired event and passes
+// it to the playRound function 
 
-function playRound () {
-    const playerSelection = playerPlay();
+let gamesPlayed = 0;
+let winsComputer = 0;
+let winsHuman = 0; 
+let draws = 0; 
+const btn = document.querySelector('#buttons');
+btn.addEventListener('click', function(e) {
+    const targetId = e.target.id;
+    switch(targetId) {
+        case 'rock':
+            playRound(targetId);
+            break;
+        case 'paper':
+            playRound(targetId);
+            break;
+        case 'scissors':
+            playRound(targetId);
+            break;
+    };
+
+    //show points, if 5 games played refresh
+    const displayPoints = document.querySelector('#currentPoints');
+    displayPoints.textContent = `Games played: ${gamesPlayed} Computer: ${winsComputer} Draws: ${draws} Human: ${winsHuman}`;
+    
+    if (gamesPlayed === 5) {
+        switch (true) {
+            case (winsComputer < winsHuman):
+                displayPoints.textContent = `You win!! Your wins: ${winsHuman} Computer wins: ${winsComputer} Draws: ${draws}`;
+                gamesPlayed = 0;
+                winsComputer = 0;
+                winsHuman = 0;
+                draws = 0;
+                break;
+            case (winsComputer > winsHuman):
+                displayPoints.textContent = `You lose! Your wins: ${winsHuman} Computer wins: ${winsComputer} Draws: ${draws}`;
+                gamesPlayed = 0;
+                winsComputer = 0;
+                winsHuman = 0;
+                draws = 0;
+                break;
+            case (winsComputer === winsHuman):
+                displayPoints.textContent = `It's a draw! Your wins: ${winsHuman} Computer wins: ${winsComputer} Draws: ${draws}`;
+                gamesPlayed = 0;
+                winsComputer = 0;
+                winsHuman = 0;
+                draws = 0;
+                break;
+        }
+    };
+});
+
+function resultDisplay (winner) {
+    //show points, if 5 games played refresh
+    // const displayPoints = document.querySelector('#currentPoints');
+    // const pointsToDisplay = document.createElement('div');
+    // pointsToDisplay.textContent = `Computer: ${winsComputer} Draws: ${draws} Human: ${winsHuman}`;
+    // displayPoints.appendChild(pointsToDisplay);
+
+
+    const displayDiv = document.querySelector('#giveResult');
+    const resultToDisplay = document.createElement('div');
+    //clear Text
+    displayDiv.textContent = 'Result: ';
+    resultToDisplay.textContent = `${winner}`;
+    displayDiv.appendChild(resultToDisplay);
+};
+
+
+
+function playRound (userInput) {
+    const playerSelection = userInput;
     const computerSelection = computerPlay();
+    // let winsComputer = 0;
+    // let winsHuman = 0;
+    // let roundsPlayed = 0;
+
+
     if (playerSelection === "scissors" && computerSelection === "scissors") {
-        console.log("It's a tie");
-        return 0;
+        resultDisplay("It's a tie");
+        // console.log("It's a tie");
+        draws++;
+        gamesPlayed++;
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
-        console.log("You loose Rock beats Scissors");
-        return -1;
+        resultDisplay("You loose Rock beats Scissors");
+        // console.log("You loose Rock beats Scissors");
+        winsComputer++;
+        gamesPlayed++;
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You win Scissors beats Paper");
-        return 1;
+        resultDisplay("You win Scissors beats Paper");
+        // console.log("You win Scissors beats Paper");
+        winsHuman++;
+        gamesPlayed++;
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("You win Rock beats Scissors");
-        return 1;
+        resultDisplay("You win Rock beats Scissors");
+        // console.log("You win Rock beats Scissors");
+        winsHuman++;
+        gamesPlayed++;
     }
     else if (playerSelection === "rock" && computerSelection === "rock") {
-        console.log("It's a tie");
-        return 0;
+        resultDisplay("It's a tie");
+        // console.log("It's a tie");
+        draws++;
+        gamesPlayed++;
     }
     else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("You loose Paper beats Rock");
-        return -1;
+        resultDisplay("You loose Paper beats Rock");
+        // console.log("You loose Paper beats Rock");
+        winsComputer++;
+        gamesPlayed++;
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
-        console.log("You loose Scissors beats Paper");
-        return -1;
+        resultDisplay("You loose Scissors beats Paper");
+        // console.log("You loose Scissors beats Paper");
+        winsComputer++;
+        gamesPlayed++;
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You win Paper beats Rock");
-        return 1;
+        resultDisplay("You win Paper beats Rock")
+        // console.log("You win Paper beats Rock");
+        winsHuman++;
+        gamesPlayed++;
     }
     else if (playerSelection === "paper" && computerSelection === "paper") {
-        console.log("It's a tie");
-        return 0;
+        resultDisplay("It's a tie")
+        // console.log("It's a tie");
+        draws++;
+        gamesPlayed++;
     }
+
+    
 }
 
 // const playerSelection = playerPlay();
@@ -78,41 +173,41 @@ function playRound () {
 // keep track on who wins 
 // console log who won the game 
 
-function game () {
-    let playerWins = 0;
-    let computerWins = 0;
-    let draws = 0;
-    // play 5 times the playround function
-    // keep track on who wins
-    for (let i = 0; i < 5; i++) {
-        let result = playRound();
-        if (result === 1) {
-            playerWins += 1;
-        }
-        else if (result === -1) {
-            computerWins += 1;
-        }
-        else {
-            draws += 1;
-        }
-    }
-    // function that logs who the result
-    function result() {
-        console.log("You won " + playerWins + " times")
-        console.log("The computer won " + computerWins + " times")
-        console.log("There were " + draws + " draws")
-    } 
-    // console log who won the game
-    if (playerWins > computerWins) {
-        console.log("\nYou won the game")
-        result();
-    }
-    else if (playerWins < computerWins) {
-        console.log("\nYou lost the game")
-        result();
-    }
-    else {
-        console.log("\nIt's a tie")
-        result();
-    }
-}
+// function game () {
+//     let playerWins = 0;
+//     let computerWins = 0;
+//     let draws = 0;
+//     // play 5 times the playround function
+//     // keep track on who wins
+//     for (let i = 0; i < 5; i++) {
+//         let result = playRound();
+//         if (result === 1) {
+//             playerWins += 1;
+//         }
+//         else if (result === -1) {
+//             computerWins += 1;
+//         }
+//         else {
+//             draws += 1;
+//         }
+//     }
+//     // function that logs who the result
+//     function result() {
+//         console.log("You won " + playerWins + " times")
+//         console.log("The computer won " + computerWins + " times")
+//         console.log("There were " + draws + " draws")
+//     } 
+//     // console log who won the game
+//     if (playerWins > computerWins) {
+//         console.log("\nYou won the game")
+//         result();
+//     }
+//     else if (playerWins < computerWins) {
+//         console.log("\nYou lost the game")
+//         result();
+//     }
+//     else {
+//         console.log("\nIt's a tie")
+//         result();
+//     }
+// }
